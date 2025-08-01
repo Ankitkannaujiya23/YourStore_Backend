780d751c6fcbc3266aba2e9a70b2b0216cd3158c
ankit <ankitkannaujiya666@gmail.com>
2025-08-02 00:48:59 +0530

    change in cart controller

--- controller/cartController.js
+++ controller/cartController.js
@@ -2,36 +2,21 @@ import { response } from "express";
 
 const cartController = {
     syncCart: async (req, res) => {
-        const { userId, cart } = req.body;
+        const { cart } = req.body;
+        const userId = req.user.id;
         console.log({ userId, cart });
         const db = req.db;
         try {
             // 1. Fetch existing cart items from DB
-            const [existingItems] = await db.execute('select * from cart_items where user_id=?', [userId]);
-            const existingMap = {};
-            for (const item of existingItems) {
-                existingMap[item.product_id] = item.quantity;
-            }
-
-            // 2. Merge guest cart into user's cart
-            for (const item of cart) {
-                const { productId, quantity } = item;
-
-                if (existingMap[productId]) {
-                    // If exists, update quantity
-                    await db.query(
-                        'UPDATE cart_items SET quantity = quantity + ? WHERE user_id = ? AND product_id = ?',
-                        [quantity, userId, productId]
-                    );
+            const [existingCart] = await db.execute('select * from cart_items where user_id=?', [userId]);
+            for (const guestItem of cart) {
+                const isCartExist = existingCart.find(item => item.product_id === guestItem.id);
+                if (isCartExist) {
+                    await db.execute('update cart_items set quantity = quantity + ? where user_id = ? and product_id=?', [guestItem.quantity, userId, guestItem.id]);
                 } else {
-                    // If not exists, insert
-                    await db.query(
-                        'INSERT INTO cart_items (user_id, product_id, quantity) VALUES (?, ?, ?)',
-                        [userId, productId, quantity]
-                    );
+                    await db.execute('insert into cart_items (user_id,product_id,quantity)values(?,?,?)', [userId, guestItem.id, guestItem.quantity]);
                 }
             }
-
             // 3. Return updated cart
             const [finalCart] = await db.query(
                 'SELECT * FROM cart_items WHERE user_id = ?',
